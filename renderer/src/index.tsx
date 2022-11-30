87d9a79a3ee1157bf328831eab0b53bb40ce68f7
Sim Boon Long <simboonlong@gmail.com>
2022-11-30 10:06:41 +0800

    feat: frame connector example (#2)

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -1,93 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
+import { FrameConnectorExample } from "./FrameConnectorExample";
+import { FramedDocumentRendererExample } from "./FramedDocumentRendererExample";
 import "./index.css";
 
-const TemplateA: React.FunctionComponent<any> = ({ document }) => {
-  const issuer = document.issuers[0].name;
-
-  return (
-    <>
-      <h3>Template A</h3>
-      <hr />
-      <h1>{issuer}</h1>
-    </>
-  );
-};
-
-const TemplateB: React.FunctionComponent<any> = ({ document }) => {
-  const { recipient, licenseNumber } = document;
-
-  return (
-    <>
-      <h3>Template B</h3>
-      <hr />
-      <h1>{recipient.name}</h1>
-      <p>{licenseNumber}</p>
-    </>
-  );
-};
-
-const TemplateC: React.FunctionComponent<any> = ({ document }) => {
-  const issuer = document.issuer.name;
-
-  return (
-    <>
-      <h3>Template C</h3>
-      <hr />
-      <h1>{issuer}</h1>
-    </>
-  );
-};
-
-const TemplateD: React.FunctionComponent<any> = ({ document }) => {
-  const { name, licenseNumber } = document.credentialSubject;
-
-  return (
-    <>
-      <h3>Template D</h3>
-      <hr />
-      <h1>{name}</h1>
-      <p>{licenseNumber}</p>
-    </>
-  );
-};
+const IS_FRAME_DOCUMENT_RENDERER = true; // toggle to see the available examples
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
-/*
- * templateRegistry
- * Accept an object consisting of Layouts. Within each Layout, they can hold an array of Views. Each View is a customisable template, rendering out OA document data to each use case.
- */
 root.render(
-  <FramedDocumentRenderer
-    templateRegistry={{
-      OA_V2: [
-        {
-          id: "view_a",
-          label: "View A",
-          template: TemplateA,
-        },
-        {
-          id: "view_b",
-          label: "View B",
-          template: TemplateB,
-        },
-      ],
-      OA_V3: [
-        {
-          id: "view_a",
-          label: "View A",
-          template: TemplateC,
-        },
-        {
-          id: "view_b",
-          label: "View B",
-          template: TemplateD,
-        },
-      ],
-    }}
-  />,
+  IS_FRAME_DOCUMENT_RENDERER ? (
+    <FramedDocumentRendererExample />
+  ) : (
+    <FrameConnectorExample />
+  ),
 );
